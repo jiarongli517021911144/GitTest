831088e829e85c367d032b0c541de80a4ceb32e7
jrl <jrl@localhost.localdomain>
2023-04-28 14:54:14 +0800

    new change

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int arr[] = {1, 2, 3, 4, 5,6};
     int size = 4;
-    cout << size;
+    cout << size << endl;
+    cout << "this is a new change" << endl;
     return 0;
 }
